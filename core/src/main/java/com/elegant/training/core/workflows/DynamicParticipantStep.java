f2816f3f3dd0a7032152644fa4e5e7eff793d83e
VAMSIKRISHNA GALANKI <hcl67703@adobe.com>
2024-03-18 11:01:26 +0530

    AEM Training From Elegant

---
+++ core/src/main/java/com/elegant/training/core/workflows/DynamicParticipantStep.java
@@ -0,0 +1,26 @@
+package com.elegant.training.core.workflows;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.day.cq.workflow.WorkflowSession;
+import com.day.cq.workflow.exec.ParticipantStepChooser;
+import com.day.cq.workflow.exec.WorkItem;
+import com.day.cq.workflow.exec.Workflow;
+import com.day.cq.workflow.metadata.MetaDataMap;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(
+        service = ParticipantStepChooser.class,
+        property = { "chooser.label="+"Dynamic Participant Workflow" }
+)
+public class DynamicParticipantStep implements ParticipantStepChooser {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DynamicParticipantStep.class);
+    @Override
+    public String getParticipant(WorkItem wi, WorkflowSession ws, MetaDataMap mdm) {
+        LOGGER.info("Dynamic Participant Step was running");
+        Workflow wf = wi.getWorkflow();
+
+        return "assining-ownership";
+    }
+}
